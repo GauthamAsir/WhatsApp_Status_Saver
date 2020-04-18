52a103edf47d7c193aaf00265841d1633678433f
GauthamAsir <gauthamasir@gmail.com>
2020-04-18 20:52:02 +0530

    Request Permissions in Splash Activity
    Signed-off-by: GauthamAsir <gauthamasir@gmail.com>

--- app/src/main/java/a/gautham/statusdownloader/SplashActivity.java
+++ app/src/main/java/a/gautham/statusdownloader/SplashActivity.java
@@ -1,19 +1,61 @@
 package a.gautham.statusdownloader;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
+import java.util.Objects;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSIONS = 1234;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Handler handler = new Handler();
+        if (!arePermissionDenied()){
+            next();
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionDenied()) {
+            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==REQUEST_PERMISSIONS && grantResults.length>0){
+            if (arePermissionDenied()){
+                // Clear Data of Application, So that it can request for permissions again
+                ((ActivityManager) Objects.requireNonNull(this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+                recreate();
+            }else {
+                next();
+            }
+        }
+    }
+
+    private void next() {
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -22,5 +64,15 @@ public class SplashActivity extends AppCompatActivity {
             }
         },1000);
 
+    }
+
+    private boolean arePermissionDenied(){
+
+        for (String permissions : PERMISSIONS){
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(),permissions) != PackageManager.PERMISSION_GRANTED){
+                return true;
+            }
+        }
+        return false;
     }
 }
