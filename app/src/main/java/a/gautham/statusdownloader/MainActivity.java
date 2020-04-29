977c687081d3b02a53ed02266a0e6906d33a063a
GauthamAsir <gauthamasir@gmail.com>
2020-04-29 14:34:23 +0530

    Update AppUpdater Library & bumped to v3.0
    Signed-off-by: GauthamAsir <gauthamasir@gmail.com>

--- app/src/main/java/a/gautham/statusdownloader/MainActivity.java
+++ app/src/main/java/a/gautham/statusdownloader/MainActivity.java
@@ -28,7 +28,7 @@ import java.io.File;
 import java.util.Objects;
 
 import a.gautham.library.AppUpdater;
-import a.gautham.library.Display;
+import a.gautham.library.helper.Display;
 import a.gautham.statusdownloader.Adapter.PageAdapter;
 import a.gautham.statusdownloader.Utils.Common;
 
